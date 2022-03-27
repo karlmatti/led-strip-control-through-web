a07d352829e70f7b0f47728b3f2f3c740aac6a57
karlmatti <28657926+karlmatti@users.noreply.github.com>
2022-03-27 15:43:29 +0300

    Remove unnecessary commands from run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo 'Starting to update project..'
-git remote -v
-git remote set-url origin git@github.com:karlmatti/led-strip-control-through-web.git
 git pull git@github.com:karlmatti/led-strip-control-through-web.git
-# sudo git pull 
 echo 'Project updated!'
+echo '#####################################################'
 echo 'Starting the server..'
 sudo python3 server.py
 
